4ebbcaa2262b85803d83a959062be7d19f392180
Angel <3006613200101@ingenieria.usac.edu.gt>
2020-10-15 20:34:29 -0600

    Rutas User

--- app.py
+++ app.py
@@ -5,16 +5,63 @@ from flask_cors import CORS
 import json
 
 # Importando modelos
+from user import User
 
 app = Flask(__name__)
 
 CORS(app)
+
+# Declaracion de objetos
+Users = []
+
+# Datos ingresados
+Users.append(User(1,'Angel', 'Ordon', 'root', 'root', 1))
+Users.append(User(2,'Diego', 'Pinto', 'diego', '123', 2))
 
 # --------------- INICIO RUTAS ---------------
 
 @app.route('/', methods=['GET'])
 def rutaInicial():
     return ("Corriendo API :D, uff")
+
+# Get users
+@app.route('/users', methods=['GET'])
+def selectAllUsers():
+    global Users
+    Data = []
+
+    for user in Users:
+        Fact = {
+            'id': user.getId(),
+            'name': user.getName(),
+            'lastname': user.getLastname(),
+            'username': user.getUsername(),
+            'password': user.getPassword(),
+            'user_type': user.getUser_type()
+        }
+        Data.append(Fact)
+    
+    answer = jsonify({'users': Data})
+
+    return (answer)
+
+# Get user
+@app.route('/users/<int:id>', methods=['GET'])
+def findUser(id):
+    global Users
+    for user in Users:
+        if user.getId() == id:
+            Fact = {
+                'id': user.getId(),
+                'name': user.getName(),
+                'lastname': user.getLastname(),
+                'username': user.getUsername(),
+                'password': user.getPassword(),
+                'user_type': user.getUser_type()
+            }
+            break
+    answer = jsonify({'message': 'User found', 'user': Fact})
+    return (answer)
 
 # --------------- FIN RUTAS ---------------
 
---
+++ user.py
@@ -0,0 +1,63 @@
+'''
+*Usuario (User)
+- id (id)
+- nombre (name)
+- apellido (lastname)
+- nombre de usuario (username)
+- contrasena (password)
+- tipo_usuario (user_type)
+'''
+
+class User:
+
+    def __init__(self, id, name, lastname, username, password, user_type):
+        self.id = id
+        self.name = name
+        self.lastname = lastname
+        self.username = username
+        self.password = password
+        self.user_type = user_type
+
+    # Metodos GET y SET
+    # id
+    def getId(self):
+        return self.id
+
+    def setId(self, id):
+        self.id = id
+
+    # name
+    def getName(self):
+        return self.name
+    
+    def setName(self, name):
+        self.name = name
+    
+    # lastname
+    def getLastname(self):
+        return self.lastname
+    
+    def setLastname(self, lastname):
+        self.lastname
+
+    # username
+    def getUsername(self):
+        return self.username
+    
+    def setUsername(self, username):
+        self.username
+
+    # password
+    def getPassword(self):
+        return self.password
+    
+    def setPassword(self, password):
+        self.password = password
+
+    # user_type
+    def getUser_type(self):
+        return self.user_type
+    
+    def setUser_type(self, user_type):
+        self.user_type = user_type
+    
